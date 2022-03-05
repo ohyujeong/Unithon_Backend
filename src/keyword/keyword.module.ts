5ea670b1081c3615cb5c96bd1f22e8245957ce87
ohyujeong <ohlife4@gmail.com>
2022-03-05 13:19:23 +0900

    [Feat] 쪽지 보낼 때, 유저 매칭

--- src/keyword/keyword.module.ts
+++ src/keyword/keyword.module.ts
@@ -6,12 +6,14 @@ import { KeywordController } from './keyword.controller';
 import { KeyWordRepository } from './keyword.repository';
 import { Message, MessageSchema } from './schemas/message.schema';
 import { UsersModule } from 'src/users/users.module';
+import { Users, UsersSchema } from 'src/users/schema/users.schema';
 
 
 @Module({
   imports:[
     MongooseModule.forFeature([{ name: KeyWord.name, schema: KeyWordSchema }]),
     MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema}]),
+    MongooseModule.forFeature([{ name: Users.name, schema: UsersSchema }]),
     UsersModule
   ],
   providers: [KeywordService, KeyWordRepository],
